598be6fe3c26ac94eeb3c1af3ecffc367cee7cad
jenerlov <jenerlo@gmail.com>
2021-04-12 15:51:47 +0200

    endpoints

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// Endpoints
+
+app.get('api/', (req, res) => {
+
+})
+
+app.post('api/', (req, res) => {
+    console.log('hej');
+})
+
+app.delete('api/', (req, res) => {
+
+})
+
+// Start server
+
+app.listen(port, () => {
+    console.log(`Server is runnin on http://localhost${port}`)
+});
